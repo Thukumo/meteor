41fdf2add10761e1f0488d047d8e1a24b048c2ac
つくも <92912896+Thukumo@users.noreply.github.com>
2025-06-01 13:47:33 +0900

    サーバをいい感じにした

---
+++ native-client/src/main.rs
@@ -0,0 +1,120 @@
+#![windows_subsystem = "windows"]
+
+use std::{sync::{Arc, Mutex}, thread};
+
+use eframe::egui::{Frame, RichText, ScrollArea, Slider};
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_title("Meteor native app")
+            .with_inner_size(eframe::egui::vec2(300.0, 400.0))
+            .with_resizable(true),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Meteor native app",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::new(App::new()))
+        }),
+    )
+}
+
+struct App {
+    messages: Arc<Mutex<Vec<String>>>,
+    url: String,
+    thread_handle: Arc<Mutex<Option<std::thread::JoinHandle<()>>>>,
+    sender: Arc<Mutex<Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>>,
+    runtime_for_sender: tokio::runtime::Runtime,
+    comment: String,
+    font_size: f32,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            messages: Arc::new(Mutex::new(Vec::new())),
+            url: String::new(),
+            thread_handle: Arc::new(Mutex::new(None)),
+            sender: Arc::new(Mutex::new(None)),
+            runtime_for_sender: tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime"),
+            comment: String::new(),
+            font_size: 16.0,
+        }
+    }
+    fn connect(&mut self) {
+        let messages_clone = self.messages.clone();
+        let connection = connect_async(format!("ws://localhost:3000/{}/api/v1/ws", self.url));
+        let sender = self.sender.clone();
+        *self.thread_handle.lock().unwrap() = Some(std::thread::spawn(move || {
+            let runtime = tokio::runtime::Runtime::new().unwrap();
+            runtime.block_on(async move {
+            let (socket, _) = connection.await.expect("Failed to connect");
+            let (write, mut read) = socket.split();
+            *sender.lock().unwrap() = Some(write);
+            loop {
+                match read.next().await {
+                    Some(Ok(Message::Text(text))) => {
+                        let mut messages = messages_clone.lock().unwrap();
+                        messages.push(text.to_string());
+                    }
+                    Some(Ok(Message::Close(_))) => break,
+                    Some(Err(_)) => break,
+                    _ => {} // tokio-tungsteniteもpingに自動で応答するらしい
+                }
+            }
+            *sender.lock().unwrap() = None;
+            });
+        }));
+    }
+    fn send_message(&mut self, message: String) {
+        if let Some(sender) = self.sender.lock().unwrap().as_mut() {
+            self.runtime_for_sender.block_on(async move {
+                sender.send(Message::Text(message.into())).await.expect("Failed to send message");
+            });
+        } else {
+            self.connect();
+            thread::sleep(std::time::Duration::from_secs(1));
+            self.send_message(message);
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        eframe::egui::CentralPanel::default().show(ctx, |ui| {
+            if self.sender.lock().unwrap().is_none() {
+                ui.label("Enter room name:");
+                ui.text_edit_singleline(&mut self.url);
+                if ui.button("Connect").clicked() {
+                    ui.close_menu();
+                    ui.label("Connecting...");
+                    self.connect();
+                }
+            } else {
+                Frame::group(ui.style()).show(ui, |ui| {
+                    ui.label(RichText::new(format!("room name: {}", self.url)).size(16.0).strong());
+                    ui.add(Slider::new(&mut self.font_size, 8.0..=46.0).text("font size"));
+                });
+                Frame::group(ui.style()).show(ui, |ui| {
+                    ui.label("Send a message:");
+                    ui.text_edit_singleline(&mut self.comment);
+                    if ui.button("Send").clicked() {
+                        self.send_message(self.comment.clone());
+                        self.comment.clear();
+                    }
+                });
+                ScrollArea::vertical().show(ui, |ui| {
+                    for message in self.messages.lock().unwrap().iter().rev() {
+                        ui.label(RichText::new(message).size(self.font_size).strong());
+                    }
+                });
+            }
+            ctx.request_repaint_after_secs(0.2); // フォーカスが外れている際も、0.2秒に1回は画面を更新する
+        });
+    }
+}
---
+++ server/src/main.rs
@@ -0,0 +1,96 @@
+use std::{collections::HashMap, sync::Arc};
+
+use axum::{extract::{ws::{Message, WebSocket}, Path, State, WebSocketUpgrade}, response::Response, Router};
+use futures_util::{SinkExt, StreamExt};
+use tokio::sync::{Mutex, broadcast};
+
+async fn ws_handler(
+    Path(room): Path<String>,
+    State(state): State<Arc<AppState>>,
+    ws: WebSocketUpgrade,
+) -> Response {
+    let sender = state.room_map.lock().await.entry(room)
+        .or_insert_with(|| broadcast::channel(100).0)
+        .clone();
+    ws.on_upgrade(async |socket| {
+        socket_handler(socket, sender).await;
+    })
+}
+async fn socket_handler(socket: WebSocket, broadcaster: broadcast::Sender<Message>) {
+    let (mut ws_sender, mut ws_receiver) = socket.split();
+    // broadcasterから送信されたメッセージを受信し、WebSocketの送信先に送る
+    let broadcaster_clone = broadcaster.clone();
+    tokio::spawn(async move {
+        let mut receiver = broadcaster_clone.subscribe();
+        while let Ok(message) = receiver.recv().await {
+            if ws_sender.send(message).await.is_err() {
+                break;
+            }
+        }
+    });
+    // クライアントからのメッセージ受信の処理
+    while let Some(message) = ws_receiver.next().await {
+        match message {
+            Ok(message@ Message::Text(_)) => { if broadcaster.send(message).is_err() { break } }
+            Ok(Message::Binary(_)) => {}
+            Ok(Message::Close(_)) => { break }
+            Err(_) => { break }
+            _ => {} // pingとかは自動で応答してくれるらしい
+        }
+    }
+}
+
+struct AppState {
+    // ルーム名をキーとした、WebSocketの送信先のベクターを保持するマップ
+    room_map: Arc<Mutex<HashMap<String, broadcast::Sender<Message>>>>,
+}
+
+#[tokio::main]
+async fn main() {
+    let app_state = Arc::new(AppState {
+        room_map: Arc::new(Mutex::new(HashMap::new())),
+    });
+
+    // 有効なクライアントの接続がないルームを定期的に削除するバックグラウンドタスク
+    {
+        let app_state = Arc::clone(&app_state);
+        tokio::spawn(async move {
+            loop {
+                tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+                let mut remove_rooms = Vec::new();
+                let mut map_lock = app_state.room_map.lock().await;
+                for (room, sender) in map_lock.iter() {
+                    /*
+                        pingを送信し、ルームの送信先が空であれば削除
+                        senderにpingを送信すると、if ws_sender.send(message).await.is_err() {break;} が発火して、
+                        有効でないwebsocket接続(及びreceiver)がdropされるはず
+                    */
+                    let _ = sender.send(Message::Ping(Vec::new().into()));
+                    //tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                    if sender.receiver_count() == 0 {
+                        println!("Removing room: {}", room);
+                        remove_rooms.push(room.clone());
+                    }
+                }
+                for room in remove_rooms {
+                    map_lock.remove(&room);
+                }
+            }
+        });
+    }
+    let app = Router::new()
+        .nest("/{room}", Router::new()
+            .nest("/api", Router::new()
+                .nest("/v1", Router::new()
+                    .route("/ws", axum::routing::get(ws_handler))
+                    // .route("/comment", axum::routing::post(post_comment))
+                )
+            )
+        )
+        .with_state(app_state)
+        .fallback_service(axum::routing::get(|| async { "404 Not Found" }));
+    let listener = tokio::net::TcpListener::bind(
+        &"0.0.0.0:3000".parse::<std::net::SocketAddr>().unwrap()
+    ).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
