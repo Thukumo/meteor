ea6a945a40fc3f89d1dc177b32d8fadb810823f5
つくも <92912896+Thukumo@users.noreply.github.com>
2025-06-08 03:13:06 +0900

    ポート変えた

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ use tower_http::services::ServeDir;
 
 use crate::{handlers::{history_handler, ws_handler}, state::AppState};
 
-const SERVICE_PORT: u16 = 3000;
+const SERVICE_PORT: u16 = 80;
 
 #[tokio::main]
 async fn main() {
