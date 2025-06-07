9f6f63e0e92f63407a1faa277ce73780d840d9af
つくも <92912896+Thukumo@users.noreply.github.com>
2025-06-08 02:55:19 +0900

    静的ファイルのホスティングのミスを修正

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ mod handlers;
 mod state;
 use std::{net::SocketAddr, sync::Arc};
 
-use axum::{http::StatusCode, response::Redirect, routing::get_service, Router};
+use axum::{http::StatusCode, routing::get_service, Router};
 use tower_http::services::ServeDir;
 
 use crate::{handlers::{history_handler, ws_handler}, state::AppState};
@@ -12,8 +12,6 @@ const SERVICE_PORT: u16 = 3000;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", axum::routing::get(|| async {Redirect::permanent("/index.html")}))
-        .route_service("/{path}", get_service(ServeDir::new("static")))
         .nest("/api", Router::new()
             .nest("/v1", Router::new()
                 .nest("/room/{room}", Router::new()
@@ -23,8 +21,8 @@ async fn main() {
                 // .route("/room_list", axum::routing::get(room_list_handler))
             )
         )
-        .with_state(Arc::new(AppState::new()))
-        .fallback_service(axum::routing::get(|| async { StatusCode::NOT_FOUND }));
+        .route_service("/{*path}", get_service(ServeDir::new("static"))).fallback(|| async { StatusCode::NOT_FOUND })
+        .with_state(Arc::new(AppState::new()));
     let listener = tokio::net::TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], SERVICE_PORT))).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
