c91e6a22a02c93418505708128ee64e2902bb76a
つくも <92912896+Thukumo@users.noreply.github.com>
2025-06-08 03:23:58 +0900

    ルートパスにリダイレクトを追加

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ mod handlers;
 mod state;
 use std::{net::SocketAddr, sync::Arc};
 
-use axum::{http::StatusCode, routing::get_service, Router};
+use axum::{http::StatusCode, response::Redirect, routing::get_service, Router};
 use tower_http::services::ServeDir;
 
 use crate::{handlers::{history_handler, ws_handler}, state::AppState};
@@ -21,6 +21,7 @@ async fn main() {
                 // .route("/room_list", axum::routing::get(room_list_handler))
             )
         )
+        .route("/", axum::routing::get(|| async { Redirect::permanent("/index.html") }))
         .route_service("/{*path}", get_service(ServeDir::new("static"))).fallback(|| async { StatusCode::NOT_FOUND })
         .with_state(Arc::new(AppState::new()));
     let listener = tokio::net::TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], SERVICE_PORT))).await.unwrap();
