7f65742198d4e2bdabb00a126d6814a857770c9d
つくも <0t0nashi0kayu@gmail.com>
2025-06-10 12:54:13 +0900

    ServeDirが勝手にindex.htmlに誘導してくれるらしい

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ mod handlers;
 mod state;
 use std::{net::SocketAddr, sync::Arc};
 
-use axum::{response::Redirect, routing::get_service, Router};
+use axum::{routing::get_service, Router};
 use tower_http::services::ServeDir;
 
 use crate::{handlers::{history_handler, ws_handler}, state::AppState};
@@ -21,7 +21,6 @@ async fn main() {
                 // .route("/room_list", axum::routing::get(room_list_handler))
             )
         )
-        .route("/", axum::routing::get(|| async { Redirect::permanent("/index.html") }))
         .fallback_service(get_service(ServeDir::new("static")))
         .with_state(Arc::new(AppState::new()));
     let listener = tokio::net::TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], SERVICE_PORT))).await.unwrap();
